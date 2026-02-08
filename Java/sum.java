64c9851c019e63b23057b7b1bf275660e1fddbd1
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-08 12:45:05 +0530

    Add Strings and sum classes to demonstrate string handling and basic arithmetic

---
+++ Java/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+    }
+}
