b3ff33974fdcf9a14be6ccff7cd667bd1a624507
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-10 23:19:43 +0530

    Add passorfail class to determine pass or fail status based on user marks

---
+++ Java/passorfail.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class passorfail {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int marks;
+        System.out.println("Enter Your marks : ");
+        marks = sc.nextInt();
+
+        String Status = ((marks)>=33) ? "Pass" : "Fail";
+        System.err.println(Status);
+        }
+}
+
+
