5aa5b0c400ba8634fbd99bf1d7bd48d2907454e0
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-10 22:10:15 +0530

    Add AssignmentOperators and LogicalOperators classes to demonstrate assignment and logical operations in Java

---
+++ Java/AssignmentOperators.java
@@ -0,0 +1,12 @@
+public class AssignmentOperators {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = a;
+        a = a+10;
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+}
---
+++ Java/LogicalOperators.java
@@ -0,0 +1,9 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        System.out.println(5>2 && 10 >8);
+        System.out.println(5<2 && 10 <8);
+
+        System.out.println(5>2 || 10 >8);
+        System.out.println(5<2 || 10 <8);
+    }
+}
