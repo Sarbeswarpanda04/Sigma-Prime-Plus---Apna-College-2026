d580109105ffa69356931e6426bec38440f1f609
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-12 21:36:36 +0530

    Add firstnNaturalNumber class to calculate the sum of the first n natural numbers based on user input

---
+++ Java/firstnNaturalNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class firstnNaturalNumber{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        int i =1;
+        while(i<=n){
+            sum = sum +i;
+            i++;
+        }
+        System.out.println("first "+n+" Natural Number's Sum Is : "+sum);
+
+
+    }
+}
