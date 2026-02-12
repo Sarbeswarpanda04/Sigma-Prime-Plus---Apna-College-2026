c24d14b4c70f6f0284500ad9029d7f6f999d137e
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-12 22:32:23 +0530

    Add reversegivennumber class to reverse the digits of a given number based on user input

---
+++ Java/reversegivennumber.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class reversegivennumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a Number : ");
+        int num = sc.nextInt();
+
+        int reverse = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            reverse = (reverse*10)+lastDigit;
+            num = num / 10;
+        }
+        System.out.println("reverse of "+num+" is "+reverse);
+    }
+}
