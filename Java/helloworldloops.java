2aaff340c56f19f51f115104344719aed1910d96
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-12 21:19:40 +0530

    Add helloworldloops and helloworldwhileloop classes to print "hello world" using for and while loops

---
+++ Java/helloworldloops.java
@@ -0,0 +1,8 @@
+public class helloworldloops {
+    public static void main(String[] args) {
+        
+        for (int i =1;i<=3;i++){
+            System.out.println("hello world");
+        }
+    }
+}
