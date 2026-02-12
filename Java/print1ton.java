5540b8a59f0bd0d7cf220189b7aee533b7982a81
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-12 21:28:28 +0530

    Add print1ton class to print numbers from 1 to n based on user input

---
+++ Java/print1ton.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class print1ton {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter value of n : ");
+        int n = sc.nextInt();
+        
+        int count = 1;
+        while(count<=n){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
