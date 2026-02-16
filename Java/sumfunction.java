11be1812b4bdf112ecd4a6d0c9b2d3815e5dec90
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-16 22:44:01 +0530

    Add sumfunction class to calculate and display the sum of two numbers

---
+++ Java/sumfunction.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class sumfunction {
+
+    public static int calculatesum(int a, int b) {  
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st Number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter 2nd Number : ");
+        int b = sc.nextInt();
+
+        int sum =calculatesum(a,b);
+        
+        System.out.println(sum);
+
+    }
+}
