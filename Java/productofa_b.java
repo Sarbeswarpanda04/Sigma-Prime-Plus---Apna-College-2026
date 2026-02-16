cef9fbaa7a13d17be53e2dd84677eaa2d96c2125
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-16 23:04:40 +0530

    Add productofa_b class to implement multiplication of two numbers

---
+++ Java/productofa_b.java
@@ -0,0 +1,12 @@
+public class productofa_b {
+    public static int multiply(int a, int b){
+        int mul = a*b;
+        return mul;
+    }
+    public static void main(String[] args) {
+        
+        int product = multiply(10,2);
+
+        System.out.println("Multiply is : "+product );
+    }
+}
