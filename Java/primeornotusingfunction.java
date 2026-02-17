5a838035bc1ad82539d6b59f81f7fe72d2c54bf8
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-17 17:48:02 +0530

    Fix prime number check logic and update test case in primeornotusingfunction class

--- Java/primeornotusingfunction.java
+++ Java/primeornotusingfunction.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
 public class primeornotusingfunction {
-    
-    public static boolean prime(int n){
-        boolean isPrime=true;
-        for(int i = 2;i<n-i;i++){
-            if(n%i==0){
+
+    public static boolean prime(int n) {
+        if (n == 2) {
+            return true;
+        }
+
+        boolean isPrime = true;
+        for (int i = 2; i < n - i; i++) {
+            if (n % i == 0) {
                 isPrime = false;
                 break;
             }
@@ -13,8 +17,9 @@ public class primeornotusingfunction {
         }
         return isPrime;
     }
+
     public static void main(String[] args) {
-        
-        System.out.print(prime(5));
+
+        System.out.print(prime(1));
     }
 }
