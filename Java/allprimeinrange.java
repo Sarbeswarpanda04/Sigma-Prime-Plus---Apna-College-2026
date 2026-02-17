769a4179d9c60892e06422c9122c6e38208aaa19
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-17 18:04:56 +0530

    Fix prime number check logic and update main method in primeornotusingfunction class; add allprimeinrange class to print all prime numbers in a given range

--- Java/allprimeinrange.java
+++ Java/allprimeinrange.java
@@ -1,25 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
+public class allprimeinrange {
 
-public class primeornotusingfunction {
-
-    public static boolean prime(int n) {
+    public static boolean isPrime(int n) {
         if (n == 2) {
             return true;
         }
-
         boolean isPrime = true;
         for (int i = 2;i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 isPrime = false;
                 break;
             }
-
         }
         return isPrime;
+    
+}
+    public static void Primeinrange(int n) {
+        for(int i =2;i<=n;i++){
+            if(isPrime(i)){
+                System.out.println(i+"");
+            }
+        }
+        System.out.println();
     }
-
     public static void main(String[] args) {
-
-        System.out.print(prime(16));
+        Primeinrange(20);
     }
 }
