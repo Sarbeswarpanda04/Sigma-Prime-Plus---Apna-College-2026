1816ff60e952d346e70135b2e5fcd5e1d01b8b7c
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-18 23:25:46 +0530

    Add Hollow_Rhombus.java to implement a method for displaying a hollow rhombus pattern

---
+++ Java/Hollow_Rhombus.java
@@ -0,0 +1,24 @@
+public class Hollow_Rhombus {
+
+    public static void Hollow_Rhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // hollow rectangle
+            for (int j = 1; j <= n; j++) {
+                // cell = (i,j)
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Hollow_Rhombus(5);
+    }
+}
