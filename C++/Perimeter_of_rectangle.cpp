47a2b93887e2d15cae51142dd1a7ce01c5ac3bb4
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-19 20:21:14 +0530

    Fix formatting and improve output for perimeter calculation in Perimeter_of_rectangle.cpp

--- C++/Perimeter_of_rectangle.cpp
+++ C++/Perimeter_of_rectangle.cpp
@@ -4,8 +4,10 @@ int main(){
     int length;
     int breadth;
     cout<<"Enter Length : ";
-    
+    cin>>length;
     cout<<"Enter Breadth : ";
-
+    cin>>breadth;
+    int perimeter = 2*(length+breadth);
+    cout<<"Perimeter is : "<<perimeter<<endl;
     return 0;
 }
