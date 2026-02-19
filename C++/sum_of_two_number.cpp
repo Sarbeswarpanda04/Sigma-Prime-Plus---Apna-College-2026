c97714f33f105667453e1ea4c84dbb84499f1b3b
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-19 20:25:08 +0530

    Add sum_of_two_number.cpp to implement a program for summing two integers

---
+++ C++/sum_of_two_number.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ,b;
+    cout<<"Input a : ";
+    cin>>a;
+    cout<<"Input b : ";
+    cin>>b;
+    int sum = a+b;
+    cout<<"a+b="<<sum<<endl;
+
+    return 0;
+}
