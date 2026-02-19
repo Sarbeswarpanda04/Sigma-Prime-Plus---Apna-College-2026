c48183f33f207ba0adafb1491cebe197970d25bf
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-19 20:32:43 +0530

    Add Area_of_square.cpp to calculate the area of a square based on user input

---
+++ C++/Area_of_square.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter sides of a square : ";
+    cin>>n;
+
+    int area = n*n;
+    cout<<"Area of the square = "<<area<<endl;
+    
+    return 0;
+}
