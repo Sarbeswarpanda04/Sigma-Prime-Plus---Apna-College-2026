4a1004c4450872df6f6f0f1037319d2ae8d8417a
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-21 00:27:18 +0530

    Add sub_array.java to implement functionality for generating and printing all subarrays

---
+++ Java/sub_array.java
@@ -0,0 +1,22 @@
+public class sub_array {
+    public static void subarray(int arr[]){
+        int ts = 0;
+        for(int  i=0;i<arr.length;i++){
+            int start = i;
+            for(int j = i;j<arr.length;j++){
+                int end = j;
+                for(int k=start;k<=end;k++){ //print
+                    System.out.print(arr[k]+" ");
+                }
+                ts++;
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println("Total Sub array : "+ts);
+    }
+    public static void main(String[] args) {
+    int arr[] = {2,4,6,8,10};
+    subarray(arr);    
+    }
+}
