772ad124d560d49b847ea716bfe71c2a7515e285
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-21 00:02:16 +0530

    Add Reverse_an_Array.java to implement array reversal in Java

--- 69.sqrt-x.java
+++ 69.sqrt-x.java
@@ -7,17 +7,21 @@
 // @lc code=start
 class Solution {
     public int mySqrt(int x) {
-        int num=1;
-        for(int i=1;i<x/2;i++){
-           num = i*i;
-        }
-        if(num<=x){
-            return num;
-        }
-        else {
-            return
-        }
+        if(x<2) return x;
+        int left =1;
+        int right = x/2;
+        int ans =0;
+        while(left<=right){
+            int mid = left+(right-left)/2;
 
+            if(mid<=x/mid){
+                ans = mid;
+                left =mid+1;
+            } else {
+                right = mid-1;
+            }
+        }
+        return ans;
     }
 }
 // @lc code=end
