c34072622ac68a29d34c3ce84182f9620d869524
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-20 21:24:43 +0530

    Add Array_input.java to demonstrate user input and percentage calculation in Java

---
+++ Java/Array_input.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Array_input {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.print("Enter Marks of Physics : ");
+        marks[0] = sc.nextInt(); //phy
+        System.out.print("Enter Marks of Math : ");
+        marks[1] = sc.nextInt(); //math
+        System.out.print("Enter Marks of Chemistry : ");
+        marks[2] = sc.nextInt(); //chem
+
+        System.out.println("phy : "+marks[0]);
+        System.out.println("Math : "+marks[1]);
+        System.out.println("Chemistry : "+marks[2]);
+
+        int percentage = (marks[0]+marks[1]+marks[2])/3;
+        System.out.println("Percentage : "+percentage );
+    }
+}
