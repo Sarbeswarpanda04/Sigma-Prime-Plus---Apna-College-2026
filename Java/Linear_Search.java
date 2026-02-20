c6ff552fc8cfde3d6838c4f1db8d8c6d3aee3965
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-20 22:32:50 +0530

    Add Linear_Search.java to implement linear search algorithm in Java

---
+++ Java/Linear_Search.java
@@ -0,0 +1,22 @@
+public class Linear_Search {
+    public static int linearsearch(int arr[], int key){
+        for(int i=0;i<=arr.length;i++){
+            if(arr[i]==key){
+              return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int key = 10;
+        int arr[] = {2,4,6,8,10,12,14,16};
+
+        int index = linearsearch(arr, key);
+        if(index== -1){
+            System.out.println("Not Found");
+        }
+        else {
+            System.out.println("Key is at index : "+index);
+        }
+    }
+}
