42f14e3b9622cef3b3e67866d9accb8af10ac9d3
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-20 21:15:30 +0530

    Add ArrayCc.java to demonstrate array initialization and output in Java

---
+++ Java/ArrayCc.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ArrayCc {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        int num[] = {1,2,3};
+        String fruits[] = {"Apple","Mango","Orange"};
+        System.out.println(Arrays.toString(num));
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(arr));
+    }
+}
