7e650a04f81e5a7662ecbbdce89ea8f3b19684c5
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-20 19:31:19 +0530

    Add operators.cpp to demonstrate arithmetic operations in C++

---
+++ C++/operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Arithmatic Operators
+    int a = 10;
+    int b = 5;
+    //addition
+    cout<<"Addition = "<<a+b<<endl;
+
+    //substraction
+    cout<<"Substraction = "<<a-b<<endl;
+
+    //Multiplication
+    cout<<"Multiplication = "<<a*b<<endl;
+
+    //Division
+    cout<<"Division = "<<a/b<<endl;
+
+    //Modulo
+    cout<<"Modulo = "<<a%b<<endl;
+
+    return 0;
+}
