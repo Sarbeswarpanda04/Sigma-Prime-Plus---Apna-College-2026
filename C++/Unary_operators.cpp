d98336102115ad16a0839e34c1af0c8306fde938
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-20 19:34:32 +0530

    Add Unary_operators.cpp to demonstrate the use of unary operators in C++

---
+++ C++/Unary_operators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    a++;
+    cout<<"A = "<<a<<endl;
+    int b = a++;
+    cout<<"B = "<<b<<endl;
+    cout<<"A = "<<a<<endl;
+
+}
