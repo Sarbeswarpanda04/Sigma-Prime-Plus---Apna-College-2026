fdb6390cffab2ba6a67c9e7126a0d8babc1ae414
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-24 21:10:02 +0530

    Add Bubble_Sort.java to implement bubble sort algorithm and print sorted array

--- Java/Bubble_Sort.java
+++ Java/Bubble_Sort.java
@@ -1,7 +1,27 @@
 public class Bubble_Sort {
-    
+    public static void Bubble_Sort(int arr[]){
+        for(int turn = 0; turn < arr.length-1; turn++){
+            for(int j = 0; j< arr.length-1-turn; j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void PrintArr(int arr[]){
+        for(int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int arr[] = {5, 4, 3, 2, 1};
         int n = arr.length;
+        Bubble_Sort(arr);
+        PrintArr(arr);
     }
 }
