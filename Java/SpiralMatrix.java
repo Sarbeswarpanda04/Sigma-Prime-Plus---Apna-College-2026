83d770bff7fdb32125285c3a5fc8dd798e1e4d9b
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-28 19:35:08 +0530

    Add SpiralMatrix.java to implement a program for inputting a 2D array

---
+++ Java/SpiralMatrix.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class SpiralMatrix {
+    
+    public static void main(String[] args) {
+        int n = 3, m = 4;
+        int arr[][] = new int[n][m];
+
+        //input
+        Scanner sc =new Scanner(System.in);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        
+
+    }
+}
