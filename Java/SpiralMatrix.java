daf435241bd624f9a8af3cf2ddc8b34806f1a8c3
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-28 19:56:54 +0530

    Refactor SpiralMatrix.java to enhance Printspiral method and update main method for consistent input size

--- Java/SpiralMatrix.java
+++ Java/SpiralMatrix.java
@@ -1,8 +1,47 @@
 import java.util.*;
 public class SpiralMatrix {
-    
+    public static void Printspiral(int arr[][]){
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = arr.length - 1;
+        int endCol = arr[0].length-1;
+
+        while(startRow <= endRow && startCol <= endCol){
+            //top
+            for(int j=startCol; j<=endCol ; j++){
+                System.out.print(arr[startRow][j]+" ");
+            }
+
+            //right
+            for(int i=startRow+1; i<=endRow ; i++){
+                System.out.print(arr[i][endCol]+" ");
+            }
+
+            //bottom
+            for(int j=endCol-1; j>=startCol ; j--){
+                if(startCol == endCol){
+                    return ;
+                }
+                System.out.print(arr[endRow][j]+" ");
+            }
+
+            //left
+            for(int i=endRow-1; i>=startRow+1 ; i++){
+                if(startRow == endRow){
+                    return;
+                }
+                System.out.print(arr[i][startCol]+" ");
+            }
+
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        int n = 3, m = 4;
+        int n = 4, m = 4;
         int arr[][] = new int[n][m];
 
         //input
@@ -13,7 +52,8 @@ public class SpiralMatrix {
             }
         }
 
+        Printspiral(arr);
         
-
+        sc.close();
     }
 }
