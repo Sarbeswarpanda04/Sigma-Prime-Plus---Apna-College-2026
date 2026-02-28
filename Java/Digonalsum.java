14afb3f0369cfdd51560255856b74192a2438077
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-02-28 20:12:06 +0530

    Refactor SpiralMatrix.java for consistent formatting and add Digonalsum.java to calculate diagonal sums in a matrix

---
+++ Java/Digonalsum.java
@@ -0,0 +1,37 @@
+public class Digonalsum {
+    public static int digonalSum(int matrix[][]){ //O(n^2)
+        int sum =0;
+
+        // for(int i=0; i<matrix[0].length;i++){
+        //     for(int j=0; j<matrix[0].length;j++){
+        //         if(i==j){
+        //             sum += matrix[i][j];
+        //         }
+        //         if(i+j == matrix.length-1){
+        //            sum += matrix[i][j]; 
+        //         }
+        //     }
+
+        // }
+ 
+        for(int i=0; i<matrix.length;i++){
+           //Primary Digonal
+           sum += matrix[i][i];
+           //Secondary Digonal
+           if(i != matrix.length-i-1)
+           sum += matrix[i][matrix.length-i-1]; 
+        }
+
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int matrix [][] = {{1, 2, 3, 4},
+                            {5, 6, 7, 8},
+                            {9, 10, 11, 12},
+                            {13, 14, 15, 16}};
+
+    System.out.println(digonalSum(matrix));
+    }
+}
--- Java/SpiralMatrix.java
+++ Java/SpiralMatrix.java
@@ -4,7 +4,7 @@ public class SpiralMatrix {
         int startRow = 0;
         int startCol = 0;
         int endRow = arr.length - 1;
-        int endCol = arr[0].length-1;
+        int endCol = arr[0].length - 1;
 
         while(startRow <= endRow && startCol <= endCol){
             //top
