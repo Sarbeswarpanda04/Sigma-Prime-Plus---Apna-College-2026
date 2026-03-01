4e81ae544e7d21803b09ca46c4ba209351b1990f
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-01 17:42:07 +0530

    Add StringFunction.java to demonstrate string length calculation

---
+++ Java/StringFunction.java
@@ -0,0 +1,7 @@
+public class StringFunction {
+    
+    public static void main(String[] args) {
+        String fullName = "Sarbeswar Panda";
+        System.out.println(fullName.length());
+    }
+}
