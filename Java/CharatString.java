051299ca9101d4ea0e9e781a698c74fffee480eb
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-01 17:47:46 +0530

    Add CharatString.java to demonstrate character access in a string

---
+++ Java/CharatString.java
@@ -0,0 +1,10 @@
+public class CharatString {
+    public static void main(String[] args) {
+        String firstName = "Sarbeswar";
+        String lastName = "Panda";
+
+        String fullName = firstName+" "+lastName;
+
+        System.out.println(fullName.charAt(0));
+    }
+}
