efb845d58d23a0f108b6737df307c166ff7be878
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-01 17:44:27 +0530

    Add ConcatenationString.java to demonstrate string concatenation in Java

---
+++ Java/ConcatenationString.java
@@ -0,0 +1,10 @@
+public class ConcatenationString {
+    public static void main(String[] args) {
+        String firstName = "Sarbeswar";
+        String lastName = "Panda";
+
+        String fullName = firstName+" "+lastName;
+
+        System.out.println(fullName);
+    }
+}
