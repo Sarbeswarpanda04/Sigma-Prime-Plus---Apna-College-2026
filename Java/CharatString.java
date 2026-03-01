291bdcccc80582631a5bc007f41592cfd3cfa9e2
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-01 18:04:05 +0530

    Add printletters method to CharatString.java to print each character of a string

--- Java/CharatString.java
+++ Java/CharatString.java
@@ -1,4 +1,10 @@
 public class CharatString {
+    public static void printletters(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.println(str.charAt(i)+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         String firstName = "Sarbeswar";
         String lastName = "Panda";
@@ -6,5 +12,6 @@ public class CharatString {
         String fullName = firstName+" "+lastName;
 
         System.out.println(fullName.charAt(0));
-    }
+        printletters(fullName);
+    }  
 }
