cfd272b6a3765a5fa9c735165852a5ffd3861660
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-01 18:28:49 +0530

    Add StringComparison.java to demonstrate string comparison using == and equals methods

---
+++ Java/StringComparison.java
@@ -0,0 +1,22 @@
+public class StringComparison {
+    
+    public static void main(String[] args) {
+        String s1 = "Tony";
+        String s2 = "Tony";
+        String s3 = new String("Tony");
+
+        if(s1==s3){
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("Not equal");
+        }
+
+        if(s1.equals(s3)){
+            System.out.println("Equal");
+        }
+        else {
+            System.out.println("Not equal");
+        }
+    }
+}
