73594935f6e703b5eab0fd4fff57634bb0cdf9e6
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-03 18:08:30 +0530

    Add OddOrEven.java to demonstrate checking if a number is odd or even using bitwise operation

---
+++ Java/OddOrEven.java
@@ -0,0 +1,11 @@
+public class OddOrEven {
+     public static void main(String[] args) {
+        int BitMask = 1;
+        if((14 & BitMask)==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+    }
+}
