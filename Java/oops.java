0ee87c8aa71bcae5c73bd40f1a4bee64ba19f60c
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-05 13:50:47 +0530

    Refactor oops.java to use getters for Pen class attributes and clean up commented code

--- Java/oops.java
+++ Java/oops.java
@@ -4,33 +4,41 @@ public class oops {
     public static void main(String[] args) {
         Pen p1 = new Pen(); //Created a pen object called p1
         p1.setColor("Blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
         p1.setTip(6);
-        System.out.println(p1.tip);
-        p1.color = "Yellow";
-        System.out.println(p1.color);
+        System.out.println(p1.getTip());
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
 
-        BankAccount myAcc = new BankAccount();
-        myAcc.username = "Sarbeswar";
-        myAcc.setPassword("abcde");
+        // BankAccount myAcc = new BankAccount();
+        // myAcc.username = "Sarbeswar";
+        // myAcc.setPassword("abcde");
 
     }
 }
 
-class BankAccount {
-    public String username;
-    private String password;
-    public void setPassword(String pwd){
-        password = pwd;
-    }
+// class BankAccount {
+//     public String username;
+//     private String password;
+//     public void setPassword(String pwd){
+//         password = pwd;
+//     }
 
-}
+// }
 class  Pen{
-     String color;
-     int tip;
+     private String color;
+     private int tip;
+
+     String getColor(){
+        return this.color;
+     }
+
+     int getTip(){
+        return this.tip;
+     }
 
      void setColor(String newColor){
-        color = newColor;
+        this.color = newColor;
      }
 
      void setTip(int newTip){
@@ -38,12 +46,12 @@ class  Pen{
      }
 }
 
-class Student {
-    String name;
-    int age;
-    float percentage; //CGPA
+// class Student {
+//     String name;
+//     int age;
+//     float percentage; //CGPA
 
-    void calcPercentage(int phy, int chem, int math){
-        percentage = (phy+chem+math)/3;
-    }
-}
+//     void calcPercentage(int phy, int chem, int math){
+//         percentage = (phy+chem+math)/3;
+//     }
+// }
