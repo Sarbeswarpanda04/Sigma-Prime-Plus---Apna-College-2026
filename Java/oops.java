7a97776a7d15a6b4b310539eef31d869230af09f
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-05 14:46:58 +0530

    Refactor oops.java to demonstrate Calculator functionality and comment out previous examples

--- Java/oops.java
+++ Java/oops.java
@@ -2,30 +2,46 @@ import javax.print.DocFlavor.STRING;
 
 public class oops {
     public static void main(String[] args) {
-        Pen p1 = new Pen(); //Created a pen object called p1
-        p1.setColor("Blue");
-        System.out.println(p1.getColor());
-        p1.setTip(6);
-        System.out.println(p1.getTip());
-        p1.setColor("Yellow");
-        System.out.println(p1.getColor());
+        // Pen p1 = new Pen(); //Created a pen object called p1
+        // p1.setColor("Blue");
+        // System.out.println(p1.getColor());
+        // p1.setTip(6);
+        // System.out.println(p1.getTip());
+        // p1.setColor("Yellow");
+        // System.out.println(p1.getColor());
 
-        // BankAccount myAcc = new BankAccount();
-        // myAcc.username = "Sarbeswar";
-        // myAcc.setPassword("abcde");
+        // // BankAccount myAcc = new BankAccount();
+        // // myAcc.username = "Sarbeswar";
+        // // myAcc.setPassword("abcde");
 
-        Student s1 = new Student("Sarbeswar");
-        System.out.println(s1.name);
+        // Student s1 = new Student("Sarbeswar");
+        // System.out.println(s1.name);
 
 
-        Fish shark = new Fish();
-        shark.eat();
-        shark.swim();
+        // Fish shark = new Fish();
+        // shark.eat();
+        // shark.swim();
 
-        Dog dobby = new Dog();
-        dobby.eat();
-        dobby.legs = 4;
-        System.out.println(dobby.legs);
+        // Dog dobby = new Dog();
+        // dobby.eat();
+        // dobby.legs = 4;
+        // System.out.println(dobby.legs);
+
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(5, 8));
+        System.out.println(calc.sum(8.5f, 2.5f));
+    }
+}
+
+class Calculator {
+    int sum(int a, int b){
+        return a + b;
+    }
+    float sum(float a, float b){
+        return a + b;
+    }
+    int sum(int a, int b, int c){
+        return a + b + c;
     }
 }
 
@@ -37,69 +53,69 @@ public class oops {
 //     }
 
 // }
-class  Pen{
-     private String color;
-     private int tip;
+// class  Pen{
+//      private String color;
+//      private int tip;
 
-     String getColor(){
-        return this.color;
-     }
+//      String getColor(){
+//         return this.color;
+//      }
 
-     int getTip(){
-        return this.tip;
-     }
+//      int getTip(){
+//         return this.tip;
+//      }
 
-     void setColor(String newColor){
-        this.color = newColor;
-     }
+//      void setColor(String newColor){
+//         this.color = newColor;
+//      }
 
-     void setTip(int newTip){
-        tip = newTip;
-     }
-}
+//      void setTip(int newTip){
+//         tip = newTip;
+//      }
+// }
 
-class Student {
-    String name;
-    int role;
+// class Student {
+//     String name;
+//     int role;
     
-    Student(String name){
-        this.name = name;
-    }
-}
-
-//Base class
-class Animal {
-    String color;
-    void eat(){
-        System.out.println("eats");
-    }
-    void breathe(){
-        System.out.println("Breathes");
-    }
-}
-
-class Mammals extends Animal {
-    int legs;
-    void walk(){
-        System.out.println("walks");
-    }
-}
-class Fish extends Animal {
-
-    void swim(){
-        System.out.println("swim");
-    }
-}
-
-class Dog extends Mammals{
-String Bread;
-}
-
-//Derived class
-// class Fish extends Animal {
-//     int fins;
-
-//     void swim(){
-//          System.out.println("Swims");
+//     Student(String name){
+//         this.name = name;
 //     }
 // }
+
+// //Base class
+// class Animal {
+//     String color;
+//     void eat(){
+//         System.out.println("eats");
+//     }
+//     void breathe(){
+//         System.out.println("Breathes");
+//     }
+// }
+
+// class Mammals extends Animal {
+//     int legs;
+//     void walk(){
+//         System.out.println("walks");
+//     }
+// }
+// class Fish extends Animal {
+
+//     void swim(){
+//         System.out.println("swim");
+//     }
+// }
+
+// class Dog extends Mammals{
+// String Bread;
+// }
+
+// //Derived class
+// // class Fish extends Animal {
+// //     int fins;
+
+// //     void swim(){
+// //          System.out.println("Swims");
+// //     }
+// // }
