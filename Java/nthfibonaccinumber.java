7dfc5021b2ed31331ffeccc9a7db83ed6e29f8dd
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-07 14:22:33 +0530

    Fix Fibonacci base case to return correct values for n = 0 and n = 1

--- Java/nthfibonaccinumber.java
+++ Java/nthfibonaccinumber.java
@@ -1,7 +1,7 @@
 public class nthfibonaccinumber {
     public static int Fibonacci(int n){
-        if(n == 1 || n == 2){
-            return 1;
+        if(n == 0 || n == 1){
+            return n;
         }
         int fibn = Fibonacci(n-1)+Fibonacci(n-2);
         return fibn;
