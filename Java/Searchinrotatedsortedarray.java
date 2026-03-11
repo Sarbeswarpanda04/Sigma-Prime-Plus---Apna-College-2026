aadf7a270fb7ad5087fc684e23d45a2106512f6a
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-11 20:40:12 +0530

    Add Searchinrotatedsortedarray class to implement search in a rotated sorted array

---
+++ Java/Searchinrotatedsortedarray.java
@@ -0,0 +1,39 @@
+public class Searchinrotatedsortedarray {
+    public static int Search(int arr[], int target, int si, int ei){
+        if(si>ei){
+            return -1;
+        }
+        //kaam
+        int mid = si+ (ei-si)/2;
+        //case found
+        if(arr[mid] == target){
+            return mid;
+        }
+        //mid on line 1
+        if(arr[si] <= arr[mid]){
+            //case a : left
+            if(arr[si] <= target && target <= arr[mid]){
+                return Search(arr, target, si, mid);
+            } else {
+                //case b : right
+                return Search(arr, target, mid+1, ei);
+            }
+        } 
+        //mid on line 2
+        else {
+            //case c : right
+            if(arr[mid] <= target && target <= arr[ei]){
+                return Search(arr, target, mid+1, ei);
+            } else {
+                //case d : left
+                return Search(arr, target, si, mid-1);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {4,5,6,7,0,1,2};
+        int target = 0;  // output = idx 4
+        int targetindex = Search(arr, target, 0, arr.length-1);
+        System.out.println(targetindex);
+    }
+}
