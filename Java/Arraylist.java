5ebc445d5a2eb2a045a76c95b7966eaee9feb11a
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-17 17:20:40 +0530

    Add Arraylist class with basic ArrayList usage example

---
+++ Java/Arraylist.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class Arraylist {
+    
+    public static void main(String[] args) {
+        
+        //ClassName objectName = new ClassName
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+
+    }
+}
