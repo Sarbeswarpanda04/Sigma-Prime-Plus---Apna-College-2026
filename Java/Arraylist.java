eb4617ff5df73d3a54147d0f4e30ff2bc8c98543
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-17 17:22:31 +0530

    Enhance Arraylist example with get and remove operations

--- Java/Arraylist.java
+++ Java/Arraylist.java
@@ -14,5 +14,15 @@ public class Arraylist {
 
         System.out.println(list);
 
+        //Get Operation
+        int element = list.get(2);
+        System.out.println(element);
+
+        //Remove Element
+        list.remove(2);
+        System.out.println(list);
+
+        
+
     }
 }
