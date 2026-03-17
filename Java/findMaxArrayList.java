6bb5a35a1572b53a60feaa6324ddf73b31cad1e4
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-17 17:50:33 +0530

    Add findMaxArrayList class to demonstrate finding the maximum element in an ArrayList

---
+++ Java/findMaxArrayList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class findMaxArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+
+
+        int Max = Integer.MIN_VALUE;
+        for(int i=0;i<list.size();i++){
+            if(list.get(i)>=Max){
+                Max = list.get(i);
+            }
+        }
+        System.out.println("Max ellement is : "+Max);
+    }
+
+}
