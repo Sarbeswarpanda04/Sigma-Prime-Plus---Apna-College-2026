1ea591c4cfe91d66961c0d2482e832df384a9762
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-17 17:59:29 +0530

    Add SortArrayList class to demonstrate sorting elements in an ArrayList

---
+++ Java/SortArrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Collection;
+public class SortArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(8);
+        list.add(2);
+        list.add(9);
+        list.add(4);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+
+    }
+}
