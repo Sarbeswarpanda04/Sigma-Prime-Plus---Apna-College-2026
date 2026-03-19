3c67017c142a4c7f610f0da5e700facf67ce6798
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-19 20:35:51 +0530

    Add removeLast method to LinkedList for improved tail removal

--- Java/LinkedList.java
+++ Java/LinkedList.java
@@ -83,6 +83,30 @@ public class LinkedList {
         return val;
     }
 
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("LL is Empty");
+            return Integer.MAX_VALUE;
+        } else if(size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        //prev : 1 = size-2
+        Node prev = head;
+        for(int i=0; i<size-2;i++){
+            prev = prev.next;
+        }
+
+        int val =  prev.next.data;  //tail.data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+
 
     public void print(){
         if(head == null){
@@ -115,5 +139,11 @@ public class LinkedList {
         ll.removeFirst();
         ll.print();
         System.out.println(ll.size);
+
+        ll.removeLast();
+        ll.print();
+        System.out.println(ll.size);
+
+
     }
 }
