61a6dc5d90876f839ef7b619ef9caace8bf36913
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-24 17:15:21 +0530

    Add Doublell class with Node inner class for doubly linked list implementation

---
+++ Java/Doublell.java
@@ -0,0 +1,20 @@
+public class Doublell {
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        } 
+    }
+    public static Node 
+    public static 
+    public static
+
+    public static void main(String[] args) {
+        
+    }
+}
