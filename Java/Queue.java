83cf8d0cf9c4f219f7d242e6c25f3c0e459417c2
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-03-28 16:52:29 +0530

    Add Queue class to implement basic queue functionality with add, remove, and peek methods

---
+++ Java/Queue.java
@@ -0,0 +1,64 @@
+public class Queue {
+    static class Queuee{
+        static int arr[];
+        static int size;
+        static int rear;
+
+        Queuee(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+
+        public static boolean isEmpty(){
+            return rear==-1;
+        }
+
+        //add
+        public static void add(int data){
+            if(rear== size-1){
+                System.out.println("Queue is full");
+                return;
+            }
+
+            rear = rear +1;
+            arr[rear] = data;
+
+        }
+
+        //remove
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+
+            int front = arr[0];
+            for(int i =0;i<rear;i++){
+                arr[i] = arr[i+1];
+            }
+            rear = rear-1;
+            return front;
+        }
+
+        //peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+    public static void main(String[] args) {
+        Queuee q = new Queuee(5);
+        q.add(0);
+        q.add(1);
+        q.add(2);
+
+        while(!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
